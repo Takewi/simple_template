0423a302d3a5a06e4c4eac13452eda9fbff878df
takewi <gawi2k21@gmail.com>
2022-03-09 22:15:18 -0300

    ✨ Add chakra provider with extended theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
+import { App } from "./components/App";
 import ReactDOM from "react-dom";
 import "./styles/tailwind.css";
-import { App } from "./components/App";
+import React from "react";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require("tailwindcss/colors");
+const extendedTheme = extendTheme({ colors });
+
+ReactDOM.render(
+    <ChakraProvider theme={extendedTheme}>
+        <App />
+    </ChakraProvider>,
+    document.getElementById("root")
+);
